12a7d20a9c635d9041d399918f0e3b9ca9cad413
Raja0514 <rajamr1987@gmail.com>
2022-08-11 10:14:55 +0400

    kkk

--- src/Pages/Chat.jsx
+++ src/Pages/Chat.jsx
@@ -1,85 +1,51 @@
-import React, { useState } from 'react'
-import './Chat.css'
-import { Link } from 'react-router-dom'
-  
-const Chat = () => {
-    const [name, setName] = useState("");
-    const [room, setRoom] = useState("");
-    return (
-        <div className="container w-25 mt-4">
-            
-            <h1 className="login-h1">Welcome</h1>
-            <form method="post">
-                <div className="form-group">
-                    <input
-                        onChange={(e) => setName(e.target.value)}
-                        type="text" placeholder="Enter your Name" required className="form-control form-input" />
-                </div>
-                <div className="form-group">
-                    <input
-                        onChange={(e) => setRoom(e.target.value)}
-                        type="text" placeholder="Enter your Chat Room Name" required className="form-control form-input" />
-                </div>
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-                <Link  onClick={e => (!name || !room) ? e.preventDefault() : null}
-                    to={`/home?name=${name}&room=${room}`}>
-                        
-                    <input type="submit" className="form-submit" value="Join" />
-                </Link>
-            </form>
-            
+const Chat = () => {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+
+  return (
+    <div className="container">
+      <div className="row mt-5 py-2">
+        <div className="col-md-3"></div>
+        <div className="col-md-6">
+          <h1 className="fs-1 text-center  p-2">Welcome to chat App</h1>
+          <form method="post">
+            <div className="mb-3">
+              
+              <input
+                type="text"
+                className="form-control"
+                onChange={(e) => setName(e.target.value)}
+                placeholder="Enter Your Name"
+              />
+            </div>
+            <div className="mb-3">
+              
+              <input
+                type="text"
+                className="form-control"
+                onChange={(e) => setRoom(e.target.value)}
+                placeholder="Enter your chat Romm Name"
+              />
+            </div>
+            <Link
+              onClick={(e) => (!name || !room ? e.preventDefault() : null)}
+              to={`/home?name=${name}&room=${room}`}
+            >
+              <input
+                type="submit"
+                className="btn btn-success w-100"
+                value="Join"
+              />
+            </Link>
+          </form>
         </div>
-    )
-}
+        <div className="col-md-3"></div>
+      </div>
+    </div>
+  );
+};
 
 export default Chat;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React from "react";
-// const handleClick = () => {
-//   localStorage.clear();
-//   window.location.reload();
-// };
-// const Chat = () => {
-//   return (
-//     <>    
-//       <button classNameName="btn btn-primary" onClick={handleClick}>
-//         logout
-//       </button>
-//     </>
-//   );
-// };
-// export default Chat;

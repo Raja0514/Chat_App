ee4c0bb3577ee93b5244a39b40bb7df2eadfd671
Raja Mathiyalagan <87691278+Raja0514@users.noreply.github.com>
2022-08-19 20:23:22 +0400

    Update Menu.jsx

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -28,7 +28,7 @@ const Menu = () => {
               className="btn btn-outline-light ms-2 px-4 rounted pill"
               to="/chat"
             >
-              join
+             hhhhhhhhhhhhhhhhhhhhhhh
 
             </Link>
 
